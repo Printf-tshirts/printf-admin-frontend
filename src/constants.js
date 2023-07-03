4f7ad9e43625f7a2926562e29560ec8e6f8c5866
Jai Malani <malanijai05@gmail.com>
2023-07-03 21:25:38 +0530

    server changed to https

--- src/constants.js
+++ src/constants.js
@@ -1,2 +1,2 @@
 // export const LOCAL_BACKEND_URL = "http://localhost:5000/api";
-export const LOCAL_BACKEND_URL = "http://server.printcoder.com:5000/api";
+export const LOCAL_BACKEND_URL = "https://server.printcoder.com/api";
